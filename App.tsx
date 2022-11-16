d368fb3c71d4e49cf0eeea112044ff4a403162cf
Guilherme Condé <guicondee@outlook.com>
2022-11-16 19:25:38 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+ 
+ 
+ export default function App() {
+  return (
+    <View>
+      <Text>
+        Vai fluir Cachorro
+      </Text>
+      <Text>
+        Só acreditar e dar seu máximo!
+      </Text>
+    </View>
+  )
+ }
